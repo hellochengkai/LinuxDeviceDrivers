4ad8ece69efe1034636ed36ac73b658af7b2d922
成凯 <hellochengkai@hotmail.com>
2019-08-05 22:46:07 +0800

    修改make脚本

--- 1/hello_world/make.sh
+++ 1/hello_world/make.sh
@@ -1,1 +1,1 @@
-make -C /usr/src/linux-headers-4.15.0-30deepin-generic/ M=/home/chengkai/Documents/GitHub/LinuxDeviceDrivers/1/hello_world modules
+make -C /lib/modules/5.0.0-23-generic/build/ M=${PWD} modules
